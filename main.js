81bd52b6c0dba375364fd2a2ec67ee4b3d2f24ab
kojirou22 <4773995+kojirou22@users.noreply.github.com>
2023-12-24 08:20:56 +0800

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e){
+        e.preventDefault();
+
+        const targetId = e.target.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scroll({
+            top: targetElement.offsetTop - 50,
+            left: 0,
+            behavior: 'smooth'
+        });
+    } 
+
+
+});
